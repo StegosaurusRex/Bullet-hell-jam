e06a6a5b7642d64af27c967811432026f1d3c01b
Vlad <pedogfd@gmail.com>
2023-04-26 16:06:42 +0800

    Initial commit

---
+++ Assets/Scripts/Player/PlayerMovements.cs
@@ -0,0 +1,19 @@
+
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal") ,
+            Input.GetAxis("Vertical"))*speed*Time.deltaTime;
+    }
+}
